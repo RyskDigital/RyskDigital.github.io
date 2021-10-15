0efc7c83b53c819e122e766f6ea6167e7e3bf65a
ryskdigital <ryskdigital@gmail.com>
2021-10-15 02:22:58 -0400

    switching to sass

--- main.js
+++ main.js
@@ -1,38 +1,37 @@
 function changeBackgroundImage() {
-    let slideElement = document.getElementsByClassName("mainPageSlides")[0];
+    let slideElement = document.getElementById("homePageSlideBlock");
     let computedStyle = getComputedStyle(slideElement);
     let currentBackgroundImage =
         computedStyle.getPropertyValue("background-image");
     switch (true) {
         case currentBackgroundImage.includes("images/Homepage_Screen_01.jpg"):
-            slideElement.style.backgroundImage =
-                "url(images/Homepage_Screen_02.jpg)";
+            slideElement.classList.remove("slide1");
+            slideElement.classList.add("slide2");
             break;
         case currentBackgroundImage.includes("images/Homepage_Screen_02.jpg"):
-            slideElement.style.backgroundImage =
-                "url(images/Homepage_Screen_03.jpg)";
+            slideElement.classList.remove("slide2");
+            slideElement.classList.add("slide3");
             break;
         case currentBackgroundImage.includes("images/Homepage_Screen_03.jpg"):
-            slideElement.style.backgroundImage =
-                "url(images/Homepage_Screen_04.jpg)";
+            slideElement.classList.remove("slide3");
+            slideElement.classList.add("slide4");
             break;
         case currentBackgroundImage.includes("images/Homepage_Screen_04.jpg"):
-            slideElement.style.backgroundImage =
-                "url(images/Homepage_Screen_05.jpg)";
+            slideElement.classList.remove("slide4");
+            slideElement.classList.add("slide5");
             break;
         case currentBackgroundImage.includes("images/Homepage_Screen_05.jpg"):
-            slideElement.style.backgroundImage =
-                "url(images/Homepage_Screen_01.jpg)";
+            slideElement.classList.remove("slide5");
+            slideElement.classList.add("slide1");
             break;
     }
 }
+
 function styleCurrentNavLink() {
-    let navLinks = document.getElementsByClassName("sidebarNavLink");
+    let navLinks = document.getElementsByClassName("sideBarNavLink");
     for (let item of navLinks) {
         if ("/" + item.getAttribute("href") == location.pathname) {
-            item.classList.add("current");
+            item.classList.add("currentPage");
         }
-        console.log(item.getAttribute("href"));
-        console.log(location.pathname);
     }
 }
