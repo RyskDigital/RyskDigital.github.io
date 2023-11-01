cbaafd549680ccbfbbc467f7bf757159b8c87dc5
ryskdigital <ryskdigital@gmail.com>
2023-11-01 02:57:41 -0400

    Cloudinary work and link hover transitions

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,6 +1,14 @@
 import "/scss/main.scss";
-import { toggleMenu, animateMenuIcon } from "/utilities.js";
+import { toggleMenu, animateMenuIcon, cloud } from "/utilities.js";
+import { name } from "@cloudinary/url-gen/actions/namedTransformation";
 
 const nav = document.querySelector(".mobile-menu-button");
 nav.addEventListener("click", toggleMenu);
 nav.addEventListener("click", animateMenuIcon);
+
+const warAxeThumb = document.querySelector(".war-axe-thumb");
+warAxeThumb.src = cloud
+    .image("portfolio-images/skyrim_steel_war_axe.jpg")
+    .namedTransformation(name("war-axe-thumb"))
+    .setSignature("GvJOB2PE")
+    .toURL();
