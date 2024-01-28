5a016d89b315d510abe2314b7774eee6890e1027
RKlimczewski <rklimczewski@ashleyfurniture.com>
2024-01-27 22:38:29 -0500

    Add contact, location, and resume link functionality

--- experience/experience.js
+++ experience/experience.js
@@ -5,3 +5,14 @@ import { name } from "@cloudinary/url-gen/actions/namedTransformation";
 const nav = document.querySelector(".mobile-menu-button");
 nav.addEventListener("click", toggleMenu);
 nav.addEventListener("click", animateMenuIcon);
+
+function open_email(email) {
+    window.location.href = `mailto:${email}${String.fromCharCode(
+        64
+    )}${String.fromCharCode(103)}mail${String.fromCharCode(46)}com`;
+}
+
+const email_link = document.querySelector(".email-link");
+email_link.addEventListener("click", function () {
+    open_email("ryskdigital");
+});
